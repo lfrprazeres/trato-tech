1de5aa553d5595dd03f596b11260579dc17e19bb
Luiz Fernando <lfrprazeres@gmail.com>
2022-08-13 15:09:19 -0300

    4.3 - criando uma action

--- src/store/reducers/itens.js
+++ src/store/reducers/itens.js
@@ -231,6 +231,16 @@ const initialState = [{
 const itensSlice = createSlice({
   name: 'itens',
   initialState,
+  reducers: {
+    mudarFavorito: (state, { payload }) => {
+      state = state.map(item => {
+        if(item.id === payload) item.favorito = !item.favorito;
+        return item;
+      })
+    }
+  }
 });
+
+export const { mudarFavorito } = itensSlice.actions;
 
 export default itensSlice.reducer;

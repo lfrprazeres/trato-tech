7a10b8f8e0da9afaa68bc6ee5463cad48bc78db3
Luiz Fernando <lfrprazeres@gmail.com>
2022-08-13 11:09:21 -0300

    1.4 - incrementando o navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,40 @@
 import styles from './Navbar.module.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
+import classNames from 'classnames';
+import {
+  RiShoppingCart2Line,
+  RiShoppingCartFill
+} from 'react-icons/ri';
+
+const iconeProps = {
+  color: 'white',
+  size: 24
+}
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <Logo className={styles.logo} />
+      <div className={styles.links}>
+        <div>
+          <a href='/' className={classNames(styles.link, {
+            [styles.selected]: window.location.pathname === '/'
+          })}>
+            Página inicial
+          </a>
+        </div>
+      </div>
+      <div className={styles.busca}>
+
+      </div>
+      <div className={styles.icones}>
+        <a href="/carrinho">
+          {window.location.pathname === '/carrinho'
+            ? <RiShoppingCartFill {...iconeProps} />
+            : <RiShoppingCart2Line {...iconeProps} />
+          }
+        </a>
+      </div>
     </nav>
   )
 }

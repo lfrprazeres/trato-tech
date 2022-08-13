29b145725734f63061814630d991c2e75d479032
Luiz Fernando <lfrprazeres@gmail.com>
2022-08-13 14:11:43 -0300

    4.1 - pegando os itens

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,10 +1,15 @@
 import Header from 'components/Header';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styles from './Categoria.module.scss';
 
 export default function Categoria() {
   const { nomeCategoria } = useParams();
-  const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === nomeCategoria));
+  const { categoria, itens } = useSelector(state => ({
+    categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
+    itens: state.itens.filter(item => item.categoria === nomeCategoria),
+  }));
+
   return (
     <div>
       <Header
@@ -12,6 +17,13 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map(item => (
+          <div key={item.id}>
+              {item.titulo}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
